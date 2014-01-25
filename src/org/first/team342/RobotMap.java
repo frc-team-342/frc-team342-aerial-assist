57342b691456889e32fdc8e71994c528dea290af
fruityfruit <hamsterbuddy101@yahoo.com>
2014-01-25 14:11:13 -0500

    Updated RobotMap with CatapultSystem

--- src/org/first/team342/RobotMap.java
+++ src/org/first/team342/RobotMap.java
@@ -1,18 +1,18 @@
 package org.first.team342;
 public class RobotMap {
-    //robot CAN drive motors
-    /*public static final int CAN_DEVICE_RIGHT_FRONT_DRIVE_MOTOR = 1;
-    public static final int CAN_DEVICE_LEFT_FRONT_DRIVE_MOTOR = 2;
-    public static final int CAN_DEVICE_LEFT_REAR_DRIVE_MOTOR = 3;
-    public static final int CAN_DEVICE_RIGHT_REAR_DRIVE_MOTOR = 4;*/
-    //robot PWM drive motors
+
+    //PORTS ARE NOT SET FOR NEW ROBOT
+    //lifto
+    public static final int PWM_CHANNEL_LIFTSYSTEM = 3;
+    //catapulto
+    public static final int PWM_CHANNEL_CATAPULTSYSTEM = 8;
+
+    //joystick ports
+    public static final int JOYSTICK_DRIVE_CONTROL = 1;
+
+    //drive mooters
     public static final int PWM_DEVICE_RIGHT_FRONT_DRIVE_MOTOR = 6;
     public static final int PWM_DEVICE_LEFT_FRONT_DRIVE_MOTOR = 5;
     public static final int PWM_DEVICE_LEFT_REAR_DRIVE_MOTOR = 4;
     public static final int PWM_DEVICE_RIGHT_REAR_DRIVE_MOTOR = 7;
-    public static final int PWM_CHANNEL_LIFTSYSTEM = 3;
-
-    //Joystick ports
-    public static final int JOYSTICK_DRIVE_CONTROL = 1;
-
 }
