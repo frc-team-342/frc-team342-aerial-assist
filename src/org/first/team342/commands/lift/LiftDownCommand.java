954e262e95dc9b967e4122f8387feac62179013a
Konnor <mcdowellkonnor@gmail.com>
2014-01-25 13:02:42 -0800

    Added Lift Commands

---
+++ src/org/first/team342/commands/lift/LiftDownCommand.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.first.team342.commands.lift;
+
+import org.first.team342.commands.CommandBase;
+import org.first.team342.subsystems.LiftSystem;
+
+/**
+ *
+ * @author Team342
+ */
+public class LiftDownCommand extends CommandBase {
+
+    private LiftSystem liftsystem;
+
+    public LiftDownCommand() {
+        this.liftsystem = LiftSystem.getInstance();
+        this.requires(this.liftsystem);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+        this.liftsystem.down();
+
+    }
+
+    protected boolean isFinished() {
+        return true;
+
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+
+}
